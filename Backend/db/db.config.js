ab15cab90180ad2ce24273cb5790dfa39bba1e5b
sohan maali <sohanmaali4@gmail.com>
2025-05-03 23:45:54 +0530

    some changes

--- Backend/db/db.config.js
+++ Backend/db/db.config.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         // await mongoose.connect("mongodb://localhost:27017/Easy_Solution");
-        await mongoose.connect('mongodb+srv://easysolution123:Xmv02488!!@easysolutioncluster.p53cg.mongodb.net/');
+        await mongoose.connect('mongodb+srv://sohaninfobeans4:SOHAN1234@aok-cluster.k6fsd.mongodb.net/Easy_solution?retryWrites=true&w=majority');
         console.log("Database connected....");
     } catch (error) {
         console.error("Database connection failed:", error);
         process.exit(1);
     }
+
 };
 
 export default connectDB;
